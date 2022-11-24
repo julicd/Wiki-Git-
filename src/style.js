28fcdeb1e0d7d81f38d64e9cfc4a6f620648f82a
julicd <89498613+julicd@users.noreply.github.com>
2022-11-24 14:24:28 -0300

    Add files via upload

---
+++ src/style.js
@@ -0,0 +1,14 @@
+import{createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+    *, body{
+        margin: 0;
+        padding: 0;
+    }
+    body{
+        backgound-color: #22272e;
+        color: #FFF;
+
+    }
+    `
+    
